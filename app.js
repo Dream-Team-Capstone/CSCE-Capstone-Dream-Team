95942eba1d7e97d24c8e30c15d050aba20a0bbb2
Amanda2English <amandaenglish3@my.unt.edu>
2025-03-03 22:25:26 -0600

    changes to run

--- app.js
+++ app.js
@@ -5,43 +5,38 @@ The application is structured to serve static files, parse request bodies, handl
 sessions, and manage certain errors.
 */
 
-
 // Load environment variables from .env file
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 // importing required Modules
-const express = require('express'); // framework to build web applications
-const app = express(); 
-const bodyParser = require('body-parser'); // Import body-parser
-const path = require('path'); // to work with file and directory paths
-const sessionConfig = require('./src/Config/config_session'); // imported from config_session file
-const { connectToDatabase } = require('./src/Config/dbh'); // imported from dbh file
-const registerRoutes = require('./src/Routes/registerRoutes'); // routes for user registration functionality
-const loginRoutes = require('./src/Routes/loginRoutes'); // routes for user login functionality
-const deleteRoutes = require('./src/Routes/deleteRoutes');
-const projectTutorialRoutes = require('./src/Routes/ProjectTutorialsRoutes');
-const deleteController = require('./src/Controllers/deleteController');
-const PORT = process.env.PORT || 4000; 
-const ejs = require('ejs'); // ejs is a templating engine for rendering HTML
-const cookieParser = require('cookie-parser'); 
-
+const express = require("express"); // framework to build web applications
+const app = express();
+const bodyParser = require("body-parser"); // Import body-parser
+const path = require("path"); // to work with file and directory paths
+const sessionConfig = require("./src/Config/config_session"); // imported from config_session file
+const { connectToDatabase } = require("./src/Config/dbh"); // imported from dbh file
+const registerRoutes = require("./src/Routes/registerRoutes"); // routes for user registration functionality
+const loginRoutes = require("./src/Routes/loginRoutes"); // routes for user login functionality
+const deleteRoutes = require("./src/Routes/deleteRoutes");
+const projectTutorialRoutes = require("./src/Routes/ProjectTutorialsRoutes");
+const deleteController = require("./src/Controllers/deleteController");
+const PORT = process.env.PORT || 4000;
+const ejs = require("ejs"); // ejs is a templating engine for rendering HTML
+const cookieParser = require("cookie-parser");
 
 // setting up views and template engine
-app.set('views', path.join(__dirname, 'src', 'Views')); 
+app.set("views", path.join(__dirname, "src", "Views"));
 // console.log('Views directory set to:', path.join(__dirname, 'src', 'Views', 'RegisterPage.ejs')); // DEBUGGING
-app.set('view engine', 'ejs'); 
+app.set("view engine", "ejs");
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'src', 'Public')));
-app.use('/node_modules', express.static('node_modules'));
-app.use(express.static(path.join(__dirname, 'src', 'blocks')));
-app.use(express.static(path.join(__dirname, 'src', 'generators')));
-app.use(express.static(path.join(__dirname, 'src', 'serialization')));
-app.use(express.static(path.join(__dirname, 'src', 'toolbox')));
-app.use(express.static(path.join(__dirname, 'src')));
-
+app.use(express.static(path.join(__dirname, "src", "Public")));
+app.use("/node_modules", express.static("node_modules"));
+app.use(express.static(path.join(__dirname, "src", "blocks")));
+app.use(express.static(path.join(__dirname, "src", "generators")));
+app.use(express.static(path.join(__dirname, "src", "serialization")));
+app.use(express.static(path.join(__dirname, "src", "toolbox")));
+app.use(express.static(path.join(__dirname, "src")));
 
 // Middleware for parsing request bodies
 app.use(express.json());
@@ -56,128 +51,128 @@ sessionConfig(app);
 connectToDatabase();
 
 // Define the home route
-app.get('/api/home', (req, res) => {
-    res.render('index'); // Renders HomePage
+app.get("/api/home", (req, res) => {
+  res.render("index"); // Renders HomePage
 });
 
 function redirectIfAuthenticated(req, res, next) {
-    // console.log('Checking auth status:', req.session); // DEBUG
-    if (req.session && req.session.userId) {
-        // console.log('User is authenticated, redirecting to dashboard'); // DEBUG
-        return res.redirect('/api/dashboard');
-    }
-    // console.log('User is not authenticated, continuing to login page'); // DEBUG
-    next();
+  // console.log('Checking auth status:', req.session); // DEBUG
+  if (req.session && req.session.userId) {
+    // console.log('User is authenticated, redirecting to dashboard'); // DEBUG
+    return res.redirect("/api/dashboard");
+  }
+  // console.log('User is not authenticated, continuing to login page'); // DEBUG
+  next();
 }
 
 // Define the login route
-app.get('/api/login', redirectIfAuthenticated, (req, res) => {
-    // console.log('Session data:', req.session); // DEBUG
-    const errors = req.session.errors || []; 
-    req.session.errors = []; // Clear errors after rendering
-    res.render('LoginPage', { 
-        errors: [],
-        user: req.session.userId ? req.session.first_name : null
-    }); 
+app.get("/api/login", redirectIfAuthenticated, (req, res) => {
+  // console.log('Session data:', req.session); // DEBUG
+  const errors = req.session.errors || [];
+  req.session.errors = []; // Clear errors after rendering
+  res.render("LoginPage", {
+    errors: [],
+    user: req.session.userId ? req.session.first_name : null,
+  });
 });
 
 // Define the play route
-app.get('/api/play', (req, res) => {
-    res.render('PlayPage'); // Renders index.html
+app.get("/api/play", (req, res) => {
+  res.render("PlayPage"); // Renders index.html
 });
 
 // Define the settings route
-app.get('/api/settings', (req, res) => {
-    res.render('SettingsPage'); // Renders SettingsPage.ejs
+app.get("/api/settings", (req, res) => {
+  res.render("SettingsPage"); // Renders SettingsPage.ejs
 });
 
 // Define the dashboard route
 function ensureAuthenticated(req, res, next) {
-    if (req.session.userId) {
-        return next();
-    }
-    res.redirect('/api/login');
+  if (req.session.userId) {
+    return next();
+  }
+  res.redirect("/api/login");
 }
-app.get('/api/dashboard', ensureAuthenticated, (req, res) => {
-    const user = req.session.first_name;
-    res.render('DashboardPage', { user });
+app.get("/api/dashboard", ensureAuthenticated, (req, res) => {
+  const user = req.session.first_name;
+  res.render("DashboardPage", { user });
 });
 
-app.get('/api/register', async (req, res) => {
-    try {
-        const errors = req.session.errors || []; 
-        req.session.errors = []; // Clear errors after rendering
-        
-        res.render('RegisterPage', { errors: [] }); // Pass errors to the view
-    } catch (err) {
-        console.error("Error rendering RegisterPage:", err);
-        return res.status(500).json({ error: 'Error rendering view' });
-    }
+app.get("/api/register", async (req, res) => {
+  try {
+    const errors = req.session.errors || [];
+    req.session.errors = []; // Clear errors after rendering
+
+    res.render("RegisterPage", { errors: [] }); // Pass errors to the view
+  } catch (err) {
+    console.error("Error rendering RegisterPage:", err);
+    return res.status(500).json({ error: "Error rendering view" });
+  }
 });
 
 // Define the logout route
-app.post('/api/logout', (req, res) => {
-
-    // Destroy session and log the user out
-    req.session.destroy((err) => {
-        if (err) {
-            // console.log('Error during session destruction:', err); // DEBUG
-            return res.status(500).send('Error logging out');
-        }
-        else{
-            res.clearCookie('connect.sid'); // clear session cookie
-            // Render the 'loggingOut.ejs' page
-            res.render('LoggingOutPage');
-        }
-    });
+app.post("/api/logout", (req, res) => {
+  // Destroy session and log the user out
+  req.session.destroy((err) => {
+    if (err) {
+      // console.log('Error during session destruction:', err); // DEBUG
+      return res.status(500).send("Error logging out");
+    } else {
+      res.clearCookie("connect.sid"); // clear session cookie
+      // Render the 'loggingOut.ejs' page
+      res.render("LoggingOutPage");
+    }
+  });
 });
 
-app.get('/api/confirmDelete', (req, res) => {
-    // console.log('Deleting account for user:', req.session); // DEBUG
-    const errors = req.session.errors || [];
-    req.session.errors = []; // Clear errors after rendering
-    res.render('DeleteAccountPage', { errors });
+app.get("/api/confirmDelete", (req, res) => {
+  // console.log('Deleting account for user:', req.session); // DEBUG
+  const errors = req.session.errors || [];
+  req.session.errors = []; // Clear errors after rendering
+  res.render("DeleteAccountPage", { errors });
 });
-
 
 // Define the delete route
-app.post('/api/delete', deleteController.deleteAccount);
+app.post("/api/delete", deleteController.deleteAccount);
 
 // Define the project tutorial route
-app.get('/api/projectTutorial', async (req, res) => {
-    if (!req.session.userId) {
-        return res.redirect('/api/login'); // Redirect if not logged in
-    }
-    
-    const userId = req.session.userId;
-    
-    // Fetch user info and progress data
-    const user_info = await user_info.findByPk(userId);
-    const user_progress = await user_progress.findOne({ where: { user_id: userId } });
+app.get("/api/projectTutorial", async (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect("/api/login"); // Redirect if not logged in
+  }
 
-    res.render('ProjectsPage', {
-        user_info: user_info,
-        user_progress: user_progress
-    });
+  const userId = req.session.userId;
+
+  // Fetch user info and progress data
+  const user_info = await user_info.findByPk(userId);
+  const user_progress = await user_progress.findOne({
+    where: { user_id: userId },
+  });
+
+  res.render("ProjectsPage", {
+    user_info: user_info,
+    user_progress: user_progress,
+  });
 });
-    
 
 // API routes
-app.use('/api', registerRoutes);
-app.use('/api', loginRoutes);
-app.use('/api', deleteRoutes);
-app.use('/api', projectTutorialRoutes);
+app.use("/api", registerRoutes);
+app.use("/api", loginRoutes);
+app.use("/api", deleteRoutes);
+app.use("/api", projectTutorialRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error("Error occurred:", err);
-    res.status(err.status || 500).json({ error: err.message || 'Something broke!' });
+  console.error("Error occurred:", err);
+  res
+    .status(err.status || 500)
+    .json({ error: err.message || "Something broke!" });
 });
 
 const startServer = () => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}/api/home`); 
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/api/home`);
+  });
 };
 
 startServer();
