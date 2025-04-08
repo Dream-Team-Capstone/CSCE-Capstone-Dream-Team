9836121522cc80badd67aa4fbfc70ede1f25136f
Amanda2English <amandaenglish3@my.unt.edu>
2025-04-07 19:24:43 -0500

    for database
    During testing there was a error with registration for the user, because the redirect to the next page is too fast. Added a 2 sec delay to allow for the database to register user and properly login/register. Added to both the registeration and the login page

--- src/Routes/loginRoutes.js
+++ src/Routes/loginRoutes.js
@@ -1,86 +1,92 @@
 // src/Routes/loginRoutes.js
 
 // import required libraries
-const express = require('express'); //framework for building the web application
-const bcrypt = require('bcryptjs'); // used to hash passwords
-const { body, validationResult } = require('express-validator'); // validate and sanitize input data
+const express = require("express"); //framework for building the web application
+const bcrypt = require("bcryptjs"); // used to hash passwords
+const { body, validationResult } = require("express-validator"); // validate and sanitize input data
 const router = express.Router(); // used to define routes
-const userModel = require('../Models/loginModel'); // model handling user data interactions with the database
-const bodyParser = require('body-parser'); // Middleware to parse incoming request bodies
-const { pool } = require('../Config/dbh'); // PostgreSQL client for database interactions
+const userModel = require("../Models/loginModel"); // model handling user data interactions with the database
+const bodyParser = require("body-parser"); // Middleware to parse incoming request bodies
+const { pool } = require("../Config/dbh"); // PostgreSQL client for database interactions
 
 // Define login route
-router.post('/login', 
-    // Validate input
-    body('email').isEmail().withMessage('Please enter a valid email.'),
-    body('password').isLength({ min: 6 }).withMessage('Please enter your password.'),
-    async (req, res) => {
-
-        // Check if user is already authenticated
-        if (req.session.userId) {
-            return res.redirect('/api/dashboard');
-        }
-
-        let errors = validationResult(req).array();
-
-        const { email, password } = req.body;
-
-        if (errors.length >= 2) {
-            errors = [{ msg: 'Must fill in ALL fields!' }];
-            return res.status(400).render('LoginPage', { errors });
-        }
-        else if (errors.length > 0){
-            return res.status(400).render('LoginPage', { errors });
-        }
-        
-        // Check for existing users
-        try {
-            const user = await userModel.getUser(email);
-            if (!user) {
-                errors.push({ msg: 'Invalid email or password.' });
-                return res.status(400).render('LoginPage', { errors });
-            }
-
-            // Compare the password
-            const trimmedPassword = password.trim(); // Trim whitespace
-            const storedHash = user.password; // Hash retrieved from DB
-
-            // Compare the provided password with the stored hash
-            const isMatch = await bcrypt.compare(trimmedPassword, user.password);
-
-            if (isMatch) {
-                // Proceed with authentication
-                req.session.userId = user.id;
-                req.session.email = user.email;
-                req.session.first_name = user.first_name;
-                req.session.last_name = user.last_name;
-                req.session.password = user.password;
-                
-                // Load user settings
-                const settingsResult = await pool.query(
-                    'SELECT dark_mode, high_contrast, font_size FROM user_settings WHERE user_id = $1',
-                    [user.id]
-                );
-                
-                if (settingsResult.rows.length > 0) {
-                    // Store settings in session
-                    req.session.userSettings = {
-                        darkMode: settingsResult.rows[0].dark_mode,
-                        highContrast: settingsResult.rows[0].high_contrast,
-                        fontSize: settingsResult.rows[0].font_size
-                    };
-                }
-            } else {
-                errors.push({ msg: 'Invalid email or password.' });
-                return res.status(400).render('LoginPage', { errors });
-            }
-            res.redirect('/api/dashboard');
-
-        } catch (error) {
-            console.error("Error logging in:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
+router.post(
+  "/login",
+  // Validate input
+  body("email").isEmail().withMessage("Please enter a valid email."),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Please enter your password."),
+  async (req, res) => {
+    // Check if user is already authenticated
+    if (req.session.userId) {
+      return res.redirect("/api/dashboard");
     }
+
+    let errors = validationResult(req).array();
+
+    const { email, password } = req.body;
+
+    if (errors.length >= 2) {
+      errors = [{ msg: "Must fill in ALL fields!" }];
+      return res.status(400).render("LoginPage", { errors });
+    } else if (errors.length > 0) {
+      return res.status(400).render("LoginPage", { errors });
+    }
+
+    // Check for existing users
+    try {
+      const user = await userModel.getUser(email);
+      if (!user) {
+        errors.push({ msg: "Invalid email or password." });
+        return res.status(400).render("LoginPage", { errors });
+      }
+
+      // Compare the password
+      const trimmedPassword = password.trim(); // Trim whitespace
+      const storedHash = user.password; // Hash retrieved from DB
+
+      // Compare the provided password with the stored hash
+      const isMatch = await bcrypt.compare(trimmedPassword, user.password);
+
+      if (isMatch) {
+        // Proceed with authentication
+        req.session.userId = user.id;
+        req.session.email = user.email;
+        req.session.first_name = user.first_name;
+        req.session.last_name = user.last_name;
+        req.session.password = user.password;
+
+        // Load user settings
+        const settingsResult = await pool.query(
+          "SELECT dark_mode, high_contrast, font_size FROM user_settings WHERE user_id = $1",
+          [user.id]
+        );
+
+        if (settingsResult.rows.length > 0) {
+          // Store settings in session
+          req.session.userSettings = {
+            darkMode: settingsResult.rows[0].dark_mode,
+            highContrast: settingsResult.rows[0].high_contrast,
+            fontSize: settingsResult.rows[0].font_size,
+          };
+        }
+
+        // Add delay before redirect
+        setTimeout(() => {
+          res.redirect("/api/dashboard");
+        }, 2000); // 2 second delay
+      } else {
+        errors.push({ msg: "Invalid email or password." });
+        return res.status(400).render("Login0age", { errors });
+      }
+      res.redirect("/api/dashboard");
+    } catch (error) {
+      console.error("Login error:", error);
+      errors.push({ msg: "Login failed. Please try again." });
+      res.status(500).render("LoginPage", { errors });
+    }
+  }
 );
 
 // export to be used in other files
--- src/Routes/registerRoutes.js
+++ src/Routes/registerRoutes.js
@@ -1,77 +1,88 @@
 // src/Routes/registerRoutes.js
 
 // Import required libraries
-const express = require('express'); // Framework for building the web application
-const bcrypt = require('bcryptjs'); // Used to hash passwords
-const { body, validationResult } = require('express-validator'); // Validate and sanitize input data
+const express = require("express"); // Framework for building the web application
+const bcrypt = require("bcryptjs"); // Used to hash passwords
+const { body, validationResult } = require("express-validator"); // Validate and sanitize input data
 const router = express.Router(); // Used to define routes
-const userModel = require('../Models/registerModel'); // Model handling user data interactions with the database
+const userModel = require("../Models/registerModel"); // Model handling user data interactions with the database
 const saltRounds = 10; // Number of salt rounds for bcrypt
 
 // Define registration route
-router.post('/register', 
-    // Validate input
-    body('first_name').notEmpty().withMessage('First name is required.'),
-    body('last_name').notEmpty().withMessage('Last name is required.'),
-    body('email').isEmail().withMessage('Invalid email address.'),
-    body('password')
-        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long!')
-        .custom((value) => {
-            // Check if password is only whitespace
-            if (value.trim().length === 0) {
-                throw new Error('Password cannot contain only whitespace!');
-            }
-            return true;
-        }),
-    async (req, res) => {
-        let errors = validationResult(req).array(); // Checks for validation errors
+router.post(
+  "/register",
+  // Validate input
+  body("first_name").notEmpty().withMessage("First name is required."),
+  body("last_name").notEmpty().withMessage("Last name is required."),
+  body("email").isEmail().withMessage("Invalid email address."),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long!")
+    .custom((value) => {
+      // Check if password is only whitespace
+      if (value.trim().length === 0) {
+        throw new Error("Password cannot contain only whitespace!");
+      }
+      return true;
+    }),
+  async (req, res) => {
+    let errors = validationResult(req).array(); // Checks for validation errors
 
-        const { first_name, last_name, email, password } = req.body;
-        const trimmedPassword = password.trim(); // Trim whitespace from password
+    const { first_name, last_name, email, password } = req.body;
+    const trimmedPassword = password.trim(); // Trim whitespace from password
 
-        // If there are validation errors, render the page with errors
-        if (errors.length >= 4) {
-            errors = [{ msg: 'Must fill in ALL fields!' }];
-            return res.status(400).render('RegisterPage', { errors });
-        }
-        else if (errors.length > 0){
-            return res.status(400).render('RegisterPage', { errors });
-        }
-
-        // Check for existing users
-        try {
-            const existingUser = await userModel.getUserByEmail(email);
-            if (existingUser) {
-                errors.push({ msg: 'User already exists.' }); // error for existing user
-                return res.status(400).render('RegisterPage', { errors });
-            }
-
-            // Generate salt
-            bcrypt.genSalt(saltRounds, (err, salt) => {
-                if (err) {
-                    console.error("Error generating salt:", err);
-                    return res.status(500).json({ error: 'Internal Server Error' });
-                }
-                
-                // Hash the password with the generated salt
-                bcrypt.hash(trimmedPassword, salt, async (err, hashedPassword) => {
-                    if (err) {
-                        console.error("Error hashing password:", err);
-                        return res.status(500).json({ error: 'Internal Server Error' });
-                    }
-
-                    // Create the new user
-                    await userModel.createUser(first_name, last_name, email, hashedPassword); 
-                    res.redirect('/api/login');
-                });
-            });
-
-        // Error handling 
-        } catch (error) {
-            console.error("Error registering user:", error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
+    // If there are validation errors, render the page with errors
+    if (errors.length >= 4) {
+      errors = [{ msg: "Must fill in ALL fields!" }];
+      return res.status(400).render("RegisterPage", { errors });
+    } else if (errors.length > 0) {
+      return res.status(400).render("RegisterPage", { errors });
     }
+
+    // Check for existing users
+    try {
+      const existingUser = await userModel.getUserByEmail(email);
+      if (existingUser) {
+        errors.push({ msg: "User already exists." }); // error for existing user
+        return res.status(400).render("RegisterPage", { errors });
+      }
+
+      // Generate salt
+      bcrypt.genSalt(saltRounds, (err, salt) => {
+        if (err) {
+          console.error("Error generating salt:", err);
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        // Hash the password with the generated salt
+        bcrypt.hash(trimmedPassword, salt, async (err, hashedPassword) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+            return res.status(500).json({ error: "Internal Server Error" });
+          }
+
+          // Create the new user
+          await userModel.createUser(
+            first_name,
+            last_name,
+            email,
+            hashedPassword
+          );
+
+          // Add delay before redirect
+          setTimeout(() => {
+            res.redirect("/api/login");
+          }, 2000); // 2 second delay
+        });
+      });
+
+      // Error handling
+    } catch (error) {
+      console.error("Error creating user:", error);
+      errors.push({ msg: "Registration failed. Please try again." });
+      res.status(500).render("RegisterPage", { errors });
+    }
+  }
 );
 
 // Export to be used in other files
