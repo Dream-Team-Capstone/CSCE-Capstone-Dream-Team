f499eff6fda1eecd95af728b98b4dcacf235608c
Amanda2English <amandaenglish3@my.unt.edu>
2025-04-11 14:00:37 -0500

    change test blockly
    Im trying to get this to work. Removed the log in section because it is unnecessary and updated how to select blocks (hopefully this works >.<)

--- cypress/e2e/blockly-workspace.cy.js
+++ cypress/e2e/blockly-workspace.cy.js
@@ -1,58 +1,56 @@
 describe("Blockly Workspace", () => {
   beforeEach(() => {
+    // Login and visit play page
     cy.login("testuser", "password123");
     cy.visit("/play");
 
-    // Wait for Blockly to be available
-    cy.window()
-      .should("have.property", "Blockly")
-      .then((win) => {
-        // Additional wait to ensure workspace is initialized
-        return new Cypress.Promise((resolve) => {
-          const checkWorkspace = () => {
-            if (win.Blockly.getMainWorkspace()) {
-              resolve();
-            } else {
-              setTimeout(checkWorkspace, 100);
-            }
-          };
-          checkWorkspace();
-        });
+    // First wait for scripts to load
+    cy.get('script[src*="blockly_compressed.js"]').should("exist");
+    cy.get('script[src*="blocks_compressed.js"]').should("exist");
+    cy.get('script[src*="python_compressed.js"]').should("exist");
+
+    // Wait for Blockly div and ensure it's visible
+    cy.get("#blocklyDiv").should("be.visible");
+
+    // Wait for Blockly to be available and initialized
+    cy.window().should("have.property", "Blockly");
+    cy.window().then((win) => {
+      return new Cypress.Promise((resolve) => {
+        function checkBlockly() {
+          const workspace = win.Blockly.getMainWorkspace();
+          if (workspace) {
+            resolve(workspace);
+          } else {
+            setTimeout(checkBlockly, 100);
+          }
+        }
+        checkBlockly();
       });
+    });
   });
 
   it("should add blocks to workspace", () => {
-    cy.get("#blocklyDiv")
-      .should("be.visible")
-      .then(() => {
-        cy.window().then((win) => {
-          // Wait for Blockly
-          cy.wrap(win)
-            .its("Blockly")
-            .should("exist")
-            .then((Blockly) => {
-              // Wait for workspace
-              const workspace = Blockly.getMainWorkspace();
-              cy.wrap(workspace).should("exist");
-            });
-        });
-      });
-
-    cy.get("#blocklyDiv").should("exist");
-
     cy.window().then((win) => {
+      expect(win.Blockly).to.exist;
       const workspace = win.Blockly.getMainWorkspace();
-      cy.wrap(workspace).should("exist");
+      expect(workspace).to.exist;
 
       // Continue with block tests
-      cy.get("[data-cy=blockly-toolbox]").should("be.visible");
-      cy.get("[data-cy=print-block]")
-        .trigger("mousedown")
+      cy.get(".blocklyToolboxContents").should("be.visible");
+      cy.get(".blocklyFlyout").should("exist");
+
+      // Try to add a block
+      cy.get(".blocklyFlyout .blocklyDraggable")
+        .first()
+        .trigger("mousedown", { force: true })
         .trigger("mousemove", { clientX: 200, clientY: 200 })
         .trigger("mouseup");
-      cy.get(".blocklyWorkspace")
-        .find(".blocklyDraggable")
-        .should("have.length", 1);
+
+      // Verify block was added
+      cy.get(".blocklyWorkspace .blocklyDraggable").should(
+        "have.length.at.least",
+        1
+      );
     });
   });
 
